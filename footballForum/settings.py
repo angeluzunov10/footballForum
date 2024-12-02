1a383336792539c94daa6d545e7f1932be9ee80a
angeluzunov10 <angel_d_u@abv.bg>
2024-12-02 23:47:44 +0200

    Made the files structure of the project. Added some static images.

--- footballForum/settings.py
+++ footballForum/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "footballForum.players.apps.PlayersConfig",
+    "footballForum.posts.apps.PostsConfig",
+    "footballForum.teams.apps.TeamsConfig",
+    "footballForum.common.apps.CommonConfig",
+    "footballForum.accounts.apps.AccountsConfig"
 ]
 
 MIDDLEWARE = [
