15ae81a97c4ac1237fba6eafb7eae4866ba65431
angeluzunov10 <angel_d_u@abv.bg>
2024-12-03 22:30:30 +0200

    Make some changes in the settings.py

--- footballForum/settings.py
+++ footballForum/settings.py
@@ -145,3 +145,7 @@ MEDIA_ROOT = BASE_DIR / 'static/images'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.AppUser'
+LOGIN_REDIRECT_URL = reverse_lazy('home')
+LOGOUT_REDIRECT_URL = reverse_lazy('home')
