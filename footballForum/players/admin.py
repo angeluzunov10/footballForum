4e9c776f423430dec7b05cd8105917fb35888c05
angeluzunov10 <angel_d_u@abv.bg>
2024-12-06 19:10:28 +0200

    Made the Players admin page.

--- footballForum/players/admin.py
+++ footballForum/players/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from footballForum.players.models import Player
 
-# Register your models here.
+
+@admin.register(Player)
+class PlayerAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'team', 'position')
+
+    search_fields = ('name', 'team__name', 'position')
+
+    ordering = ('pk',)
+
+    fieldsets = (
+        ('Personal info', {'fields': ('name', 'player_picture')}),
+        ('Club info', {'fields': ('team', 'position')}),
+    )
