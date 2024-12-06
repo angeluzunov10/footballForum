e8f8585783bc8e2152ecf8465ff68903b02e3d01
angeluzunov10 <angel_d_u@abv.bg>
2024-12-06 19:04:08 +0200

    Made the Posts admin page.

--- footballForum/posts/admin.py
+++ footballForum/posts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
+from footballForum.posts.models import Post
+
+
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk',
+        'title',
+        'author',
+        'created_at',
+        'updated_at',
+        'approved',
+    )
+
+    search_fields = ('author', 'content')
+
+    ordering = ('pk',)
+
+    fieldsets = (
+        ('Post info', {'fields': ('author', 'title', 'approved')}),
+        ('Tags', {'fields': ('tagged_teams', 'tagged_players',)}),
+        ('Content', {'fields': ('image', 'content',)}),
+        ('Important dates', {'fields': ('created_at', 'updated_at')}),
+    )
+
+    readonly_fields = ('author', 'title', 'image', 'content', 'created_at', 'updated_at',)
