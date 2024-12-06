0fbb51d48a638d2f3887a6e481e059c1fd41a4c2
angeluzunov10 <angel_d_u@abv.bg>
2024-12-06 19:14:39 +0200

    Made the Teams admin page.

--- footballForum/teams/admin.py
+++ footballForum/teams/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Team
 
-# Register your models here.
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'stadium', 'foundation_year',)
+
+    search_fields = ('name',)
+
+    ordering = ('pk',)
+
+    fieldsets = (
+        ('Team info', {'fields': ('name', 'foundation_year')}),
+        ('Other info', {'fields': ('logo', 'stadium')}),
+    )
